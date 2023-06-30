b98e8c98bd741cd683808b562dbadc5b4ae6adf8
Cooper <archett0@outlook.com>
2023-06-30 20:40:02 +0800

    Fix the way to fetch images, add function to abort

--- app/src/main/java/sg/edu/nus/iss/thememorygame/activity/FetchActivity.java
+++ app/src/main/java/sg/edu/nus/iss/thememorygame/activity/FetchActivity.java
@@ -2,8 +2,8 @@ package sg.edu.nus.iss.thememorygame.activity;
 
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
 import android.os.Bundle;
-import android.os.Handler;
 import android.util.Log;
 import android.util.LruCache;
 import android.view.View;
@@ -32,9 +32,7 @@ public class FetchActivity extends AppCompatActivity implements View.OnClickList
     public TextView progressText;
     private static LruCache<String, Bitmap> imageCache;
     private static final int imageMaxCount = 20;
-    private static final int delay = 200;
-    private final Handler setImageHandler = new Handler();
-    final Runnable executeSetImagesFromCache = this::setImagesFromCache;
+    private FetchImagesTask fetchImagesTask;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -65,19 +63,70 @@ public class FetchActivity extends AppCompatActivity implements View.OnClickList
         Toast.makeText(FetchActivity.this, msg, Toast.LENGTH_SHORT).show();
     }
 
-    public void fetchImages(String targetUrl) {
-        // TODO: remove this later
-        targetUrl = "https://stocksnap.io/";
-        Log.d("IMG_TEST", "Received image url: " + targetUrl);
-        Log.i("IMG_TEST", "Ready to fetch");
+    public void clearAllImage() {
+        for (int i = 1; i <= imageMaxCount; i++) {
+            // remove from cache
+            imageCache.remove("image" + i);
+            // clear imageview
+            int imageId = getResources().getIdentifier("image" + i, "id", getPackageName());
+            Log.i("IMG_TEST", "Removing image view id: " + imageId);
+            ImageView imageView = findViewById(imageId);
+            imageView.setImageResource(R.drawable.img_clear);
+            // clear progress bar
+            progressBar.setProgress(0);
+            // update text progress
+            String textProgress = "Downloading ? of 20 images";
+            progressText.setText(textProgress);
+        }
+    }
 
-        String finalTargetUrl = targetUrl;
-        new Thread(() -> {
+    @Override
+    public void onClick(View view) {
+        clearAllImage();
+        EditText url_input = (EditText) findViewById(R.id.url_input);
+        String target_url = String.valueOf(url_input.getText());
+
+        // entering submit phase
+        if (view.getId() == R.id.url_submit) {
+            Log.d("IMG_TEST", "Sending fetch request to: " + target_url);
+            // make download progress bar visible
+            progressBar.setVisibility(View.VISIBLE);
+            progressText.setVisibility(View.VISIBLE);
+            // check and abort the previous task
+            if (fetchImagesTask != null && fetchImagesTask.getStatus() == AsyncTask.Status.RUNNING) {
+                fetchImagesTask.cancel(true);
+            }
+            // fetch images
+            fetchImagesTask = new FetchImagesTask();
+            fetchImagesTask.execute(target_url);
+        }
+    }
+
+    /**
+     * Task to fetch images
+     */
+    private class FetchImagesTask extends AsyncTask<String, Integer, Void> {
+
+        /**
+         * Main task to fetch image
+         *
+         * @param strings params
+         * @return void
+         */
+        @Override
+        protected Void doInBackground(String... strings) {
+            String targetUrl = strings[0];
+            targetUrl = "https://stocksnap.io/";  // TODO: remove this line after testing
+            Log.d("IMG_TEST", "Received image url: " + targetUrl);
+            Log.i("IMG_TEST", "Ready to fetch");
             try {
-                Document doc = Jsoup.connect(finalTargetUrl).get();
+                Document doc = Jsoup.connect(targetUrl).get();
                 Elements imgElements = doc.select("img[src]");
                 int count = 0;
                 for (Element imgElement : imgElements) {
+                    if (isCancelled()) {
+                        break;
+                    }
                     String imageUrl = imgElement.absUrl("src");
                     // filter svg images
                     if (!imageUrl.endsWith("svg")) {
@@ -89,6 +138,8 @@ public class FetchActivity extends AppCompatActivity implements View.OnClickList
                         // confirm process of image, set into image view
                         if (bitmap != null) {
                             imageCache.put("image" + (count + 1), bitmap);
+                            // update progress bar while put image into imageview
+                            publishProgress(count + 1);
                             count++;
                         }
                         if (count == imageMaxCount) {
@@ -97,48 +148,34 @@ public class FetchActivity extends AppCompatActivity implements View.OnClickList
                     }
                 }
                 Log.i("IMG_TEST", "Fetch completed");
-                setImageHandler.post(executeSetImagesFromCache);
             } catch (Exception e) {
                 Log.d("IMG_TEST", "Error stack: " + Arrays.toString(e.getStackTrace()));
                 Log.d("IMG_TEST", "Error msg: " + e);
             }
-        }).start();
-    }
+            return null;
+        }
 
-    public void setImagesFromCache() {
-        Log.i("IMG_TEST", "Start setting images");
-        int currentProgress = 0;
-        for (int i = 1; i <= imageMaxCount; i++) {
-            // get image reference
-            int imageId = getResources().getIdentifier("image" + i, "id", getPackageName());
+        /**
+         * After image is fetched from server, load the image into the view
+         * and save it to the lru cache
+         *
+         * @param values params
+         */
+        @Override
+        protected void onProgressUpdate(Integer... values) {
+            int currentProgress = values[0];
+            // update image view
+            int imageId = getResources().getIdentifier("image" + currentProgress, "id", getPackageName());
             Log.i("IMG_TEST", "Setting image view id: " + imageId);
             ImageView imageView = findViewById(imageId);
             // set image view
-            imageView.setImageBitmap(imageCache.get("image" + i));
+            imageView.setImageBitmap(imageCache.get("image" + currentProgress));
             // update progress bar
-            currentProgress++;
-            int progress = (int) ((currentProgress / (float) imageMaxCount) * 100);
-            progressBar.setProgress(progress);
+            int progressPercentage = (int) ((currentProgress / (float) imageMaxCount) * 100);
+            progressBar.setProgress(progressPercentage);
             // update text progress
-            String textProgress = "Downloading " + i + " of " + imageMaxCount + " images";
+            String textProgress = currentProgress != imageMaxCount ? ("Downloading " + currentProgress + " of " + imageMaxCount + " images") : "Download completed";
             progressText.setText(textProgress);
-        }
-        Log.i("IMG_TEST", "Image set completed");
-    }
-
-    @Override
-    public void onClick(View view) {
-        EditText url_input = (EditText) findViewById(R.id.url_input);
-        String target_url = String.valueOf(url_input.getText());
-
-        // entering submit phase
-        if (view.getId() == R.id.url_submit) {
-            Log.d("IMG_TEST", "Sending fetch request to: " + target_url);
-            // Make download progress bar visible
-            progressBar.setVisibility(View.VISIBLE);
-            progressText.setVisibility(View.VISIBLE);
-            // fetch images
-            fetchImages(target_url);
         }
     }
 }
