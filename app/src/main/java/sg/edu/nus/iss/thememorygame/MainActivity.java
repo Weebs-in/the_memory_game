c9533999d7d9b1d65db1ea8600b046bf3cf09d13
zongyuan <chia_z_y89@hotmail.com>
2023-07-04 15:45:46 +0800

    Added in music for fetch screen and guess screen
    (Tentatively)Also input the sounds for matching pictures (winner/loser) (To be input in once the code is ready)

--- app/src/main/java/sg/edu/nus/iss/thememorygame/MainActivity.java
+++ app/src/main/java/sg/edu/nus/iss/thememorygame/MainActivity.java
@@ -3,6 +3,7 @@ package sg.edu.nus.iss.thememorygame;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.View;
 
@@ -11,6 +12,7 @@ import sg.edu.nus.iss.thememorygame.activity.GuessActivity;
 
 public class MainActivity extends AppCompatActivity {
 
+    MediaPlayer player;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,11 +20,33 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void openFetchActivity(View view) {
+        if(player == null){
+            player = MediaPlayer.create(this, R.raw.fetch_epic_dramatic_action_trailer);
+            player.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+                @Override
+                public void onCompletion(MediaPlayer mediaPlayer) {
+                    mediaPlayer.release();
+                    player=null;
+                }
+            });
+        }
+        player.start();
         Intent intent = new Intent(this, FetchActivity.class);
         startActivity(intent);
     }
 
     public void openGuessActivity(View view) {
+        if(player == null){
+            player = MediaPlayer.create(this, R.raw.guess_stranger_things);
+            player.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+                @Override
+                public void onCompletion(MediaPlayer mediaPlayer) {
+                    mediaPlayer.release();
+                    player=null;
+                }
+            });
+        }
+        player.start();
         Intent intent = new Intent(this, GuessActivity.class);
         startActivity(intent);
     }
